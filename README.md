224db548f03c950b3d4136b7e7fc0aa07a8cbba9
jporter74 <jporter74@gatech.edu>
2023-04-27 13:58:02 -0400

    Change dashboard css and readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # To start app: npm run dev
+# Remember to run the command "npm i" in the terminal before to install teh necessary packages
--- src/pages/components/Dashboards.jsx
+++ src/pages/components/Dashboards.jsx
@@ -1,6 +1,8 @@
 import React, {useRef, useEffect} from 'react'
 import HeaderAndNavBar from './HeaderAndNavBar/HeaderAndNavBar';
 
+import './Dashboards.css';
+
 const { tableau } = window;
 
 function BasicEmbed() {
@@ -26,7 +28,11 @@ function BasicEmbed() {
     return (
     <div>
         <HeaderAndNavBar/>
-        <div ref={vizRef}></div>
+        <h1 className='description'>This dashboard is here to represent the observalbe deaths of the top five causes recorded in the United States of America. The purpose of these
+          graphs is to raise awareness of the leading factors in the U.S. and to show trends within the country. This visualization is a useful tool for tracking 
+          different types of deaths among the various states and age ranges and identifying problem areas where help is needed.
+        </h1>
+        <div className='dash' ref={vizRef}></div>
     </div>
     )
 }
